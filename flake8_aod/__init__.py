6e56303aff7db9664d65a22e7ae487a3a1e8ddbf
DanielKusyDev <daniel.kusy97@gmail.com>
2022-09-17 11:09:09 +0200

    Create first version of syntax checker.

--- flake8_aod/__init__.py
+++ flake8_aod/__init__.py
@@ -1,1 +1,3 @@
 from .plugin import Plugin
+
+__all__ = [Plugin]
--- flake8_aod/plugin.py
+++ flake8_aod/plugin.py
@@ -1,4 +1,5 @@
 import abc
+import re
 import sys
 from dataclasses import dataclass
 from enum import Enum
@@ -23,26 +24,54 @@ class ErrorCode(str, Enum):
 
 @dataclass(frozen=True)
 class FailedCheck:
-    line: int
+    line: str
     match_col: int
-    line_num: str
+    line_num: int
     code: ErrorCode
     critical: bool = False
 
 
 class Checker(abc.ABC):
     @abc.abstractmethod
-    def check_line(self, line: str, line_num: int) -> FailedCheck:
+    def check_line(self, line: str, line_num: int) -> Optional[FailedCheck]:
         raise NotImplementedError
 
 
 class SyntaxChecker(Checker):
-    def check_line(self, line: str, line_num: int) -> FailedCheck:
-        pass
+    def __init__(self) -> None:
+        self._main_pattern = re.compile(r"# (ado: )|(\stodo:)", flags=re.IGNORECASE)
+
+    def check_line(self, line: str, line_num: int) -> Optional[FailedCheck]:
+        regex_result = self._main_pattern.findall(line)
+        line_num += 1
+        if regex_result:
+            tags = {part.lower() for match in regex_result for part in match if part}
+            for tag in tags:
+                reference_pattern = re.compile(rf"{tag}AB#(\d+)(\s|.$)", flags=re.IGNORECASE)
+                ado_pattern = re.compile(r"# ADO: ", flags=re.IGNORECASE)
+                number_match = re.search(reference_pattern, line)
+                ado_match = re.search(ado_pattern, line)
+                if not number_match:
+
+                    return FailedCheck(
+                        line=line,
+                        match_col=ado_match.regs[0][-1],
+                        line_num=line_num,
+                        code=ErrorCode.ADO002_MALFORMED_ITEM_REF,
+                        critical=True,
+                    )
+                if not number_match[0].isupper():
+                    return FailedCheck(
+                        line=line,
+                        match_col=ado_match.regs[0][-1],
+                        line_num=line_num,
+                        code=ErrorCode.ADO003_CAPITALIZATION,
+                        critical=False,
+                    )
 
 
 class ADOChecker(Checker):
-    def check_line(self, line: str, line_num: int) -> FailedCheck:
+    def check_line(self, line: str, line_num: int) -> Optional[FailedCheck]:
         pass
 
 
@@ -56,14 +85,10 @@ class Plugin:
         self._ado_checker = ADOChecker()
 
     def _get_error(self, failed_check: FailedCheck) -> PluginError:
-        return failed_check.line, failed_check.match_col, failed_check.code.value, type(self)
+        yield failed_check.line_num, failed_check.match_col, failed_check.code.value, type(self)
 
     def run(self) -> Generator[PluginError, None, None]:
         for i, line in enumerate(self._lines):
             syntax_error = self._syntax_checker.check_line(line, i)
             if syntax_error:
                 yield from self._get_error(syntax_error)
-            if not syntax_error or not syntax_error.critical:
-                ado_error = self._ado_checker.check_line(line, i)
-                if ado_error:
-                    yield from self._get_error(ado_error)
