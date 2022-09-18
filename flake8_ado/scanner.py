7f62e0014aaf5b94649d4bce13c0421c9d0b443f
DanielKusyDev <daniel.kusy97@gmail.com>
2022-09-18 15:36:05 +0200

    Add arguments to lru_cache,

--- flake8_ado/scanner.py
+++ flake8_ado/scanner.py
@@ -6,16 +6,16 @@ _ANNOTATION_PATTERN = re.compile(r"# ((ado: )|(todo: )).*$", flags=re.IGNORECASE
 
 
 class RegexMatcher:
-    @lru_cache
+    @lru_cache(maxsize=None)
     def get_reference_match(self, line: str, tag: str) -> re.Match:
         reference_pattern = re.compile(rf"# {tag}AB#(\d+)(\s|.$)", flags=re.IGNORECASE)
         return re.search(reference_pattern, line)
 
-    @lru_cache
+    @lru_cache(maxsize=None)
     def get_reference_number(self, line: str) -> str:
         return re.compile(r"AB#(\d+)", flags=re.IGNORECASE).findall(line)[0]
 
-    @lru_cache
+    @lru_cache(maxsize=None)
     def get_tag_match(self, line: str, tag: str) -> re.Match:
         tag_pattern = re.compile(rf"# {tag}", flags=re.IGNORECASE)
         return re.search(tag_pattern, line)
