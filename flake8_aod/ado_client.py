ca3a96254b1da9b18bdedce0c8271977cf499891
DanielKusyDev <daniel.kusy97@gmail.com>
2022-09-18 13:46:01 +0200

    Install mocker.

--- flake8_aod/ado_client.py
+++ flake8_aod/ado_client.py
@@ -12,7 +12,7 @@ class AzureDevOpsClient:
         connection = Connection(base_url=ADO_ORGANIZATION_URL, creds=credentials)
         self._client = connection.clients.get_work_item_tracking_client()
 
-    def get_not_exiting_item_ids(self, ids: List[str]) -> List[str]:
+    def get_not_existing_item_ids(self, ids: List[str]) -> List[str]:
         work_items = self._client.get_work_items(ids=ids, error_policy="Omit")
         not_existing_ids = set(ids) - {item.id for item in work_items if item}
         return list(not_existing_ids)
